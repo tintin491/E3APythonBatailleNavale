26cd3f2a73e6bc535d0c4c8a99bb603e2414ea0b
quentin <quentinkimppienne@gmail.com>
2024-10-21 14:02:10 +0200

    Début classe player

---
+++ classes/player.py
@@ -0,0 +1,13 @@
+class Player:
+
+    def __init__(self, name, liste_ship):
+            self.name = name
+            self.name = liste_ship
+
+
+
+    def placer_bateau(self, bateau):
+        return
+
+    def chercher_bateau(self, bateau):
+        return
