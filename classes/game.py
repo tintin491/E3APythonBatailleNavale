bc5a3d532e7a20ea00de595a5cfbf180ec495ad0
louison <louison.feuvrais@gmail.com>
2024-10-21 16:07:46 +0200

    setup de la game

---
+++ classes/game.py
@@ -0,0 +1,13 @@
+from classes.player import Player
+from classes.ship import Ship
+
+
+class Game:
+
+    def __init__(self):
+
+        nameOfP1 = input("Saisir le nom du joueur 1 : ")
+        nameOfP2 = input("Saisir le nom du joueur 2 : ")
+
+        playerOne = Player(nameOfP1)
+        playerTwo = Player(nameOfP2)
--- classes/player.py
+++ classes/player.py
@@ -1,7 +1,22 @@
 class Player:
-    def __init__(self, name, liste_ship):
+    def __init__(self, name):
+
+        ship1 = Ship(5, 'Horizontal')
+        ship2 = Ship(4, 'Horizontal')
+        ship3 = Ship(3, 'Horizontal')
+        ship4 = Ship(3, 'Horizontal')
+        ship5 = Ship(2, 'Horizontal')
+
+        listOfShip = [
+            ship1,
+            ship2,
+            ship3,
+            ship4,
+            ship5
+        ]
+
             self.name = name
-            self.name = liste_ship
+            self.listShip = listOfShip
 
     def placer_bateau(self, bateau):
         return
