8fb689afbc078366181c8d54f7ef3e6a63ee2559
quentin <quentinkimppienne@gmail.com>
2024-10-21 14:26:02 +0200

    Début classe player

--- classes/player.py
+++ classes/player.py
@@ -1,10 +1,7 @@
 class Player:
-
     def __init__(self, name, liste_ship):
             self.name = name
             self.name = liste_ship
-
-
 
     def placer_bateau(self, bateau):
         return
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 
+
 fenetre = Tk()
 fenetre.geometry("300x200")  # Définit la taille de la fenêtre
 
@@ -7,4 +8,7 @@ label = Label(fenetre, text="Hello World", font=("Helvetica", 16))
 label.pack()
 
 fenetre.mainloop()
+
+
+
 
