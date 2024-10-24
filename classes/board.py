b946f91e5fee07661ba0f39e2121636947091fda
quentin <quentinkimppienne@gmail.com>
2024-10-24 09:46:47 +0200

    correction bug graphique

--- classes/board.py
+++ classes/board.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 
+
 class Board:
 
     def __init__(self):
@@ -7,95 +8,87 @@ class Board:
         self.buttonsPlayerTour = []  # Stocker les boutons du joueur dont c'est le tour
         self.buttonsPlayerNonTour = []  # Stocker les boutons du joueur dont ce n'est pas le tour
         self.create_widgets()
-        self.ship = [] # Stocker les bateaux et leurs positions
-        self.player = 1
+        self.player = 1  # Débuter avec le joueur 1
         self.start = False
-        self.buttonframe1 = None
-        self.buttonframe2 = None
 
     def create_widgets(self):
         # Ajouter un label
         self.fenetre.geometry("500x600")
         labelTitre = tk.Label(self.fenetre, text="Bataille Navale", font=("Helvetica", 16))
         labelTitre.pack()
-        self.buttonframe1 = tk.Frame(self.fenetre)
-        self.buttonframe2 = tk.Frame(self.fenetre)
 
-        label1 = tk.Label(self.fenetre, text="Plateau adverse", font=("Helvetica", 16))
-        label1.pack(pady=(25,0))
+        # Création des frames pour les deux grilles
+        self.buttonframe1 = tk.Frame(self.fenetre)  # Plateau du joueur
+        self.buttonframe2 = tk.Frame(self.fenetre)  # Plateau adverse
 
+        # Label pour le plateau adverse
+        label1 = tk.Label(self.fenetre, text="Vos navires", font=("Helvetica", 16))
+        label1.pack(pady=(25, 0))
+
+        # Créer les boutons pour le plateau adverse
         for i in range(5):
             row = []
             for j in range(10):
-                btn2 = tk.Button(self.buttonframe2, text="", command=lambda a=i, b=j: self.button_change(a, b), height=self.fenetre.winfo_height() * 2, width=self.fenetre.winfo_width() * 5)
+                btn2 = tk.Button(self.buttonframe2, text="", command=lambda a=i, b=j: self.button_change(a, b), width=5,
+                                 height=2)
                 btn2.grid(row=i, column=j)
                 row.append(btn2)
             self.buttonsPlayerNonTour.append(row)
-        self.buttonframe2.pack(padx = (25, 0), fill='x')
+        self.buttonframe2.pack(padx=(25, 0), fill='x')
 
-        label2 = tk.Label(self.fenetre, text="Vos navire", font=("Helvetica", 16))
-        label2.pack(pady=(25,0))
+        # Label pour vos navires
+        label2 = tk.Label(self.fenetre, text="Plateau adverse", font=("Helvetica", 16))
+        label2.pack(pady=(25, 0))
 
+        # Créer les boutons pour le plateau du joueur
         for i in range(5):
             row = []
             for j in range(10):
-                btn1 = tk.Button(self.buttonframe1, text="", command=lambda a=i, b=j: self.button_change(a, b), height=self.fenetre.winfo_height() * 2, width=self.fenetre.winfo_width() * 5)
+                btn1 = tk.Button(self.buttonframe1, text="", command=lambda a=i, b=j: self.button_change(a, b), width=5,
+                                 height=2)
                 btn1.grid(row=i, column=j)
                 row.append(btn1)
             self.buttonsPlayerTour.append(row)
-        self.buttonframe1.pack(padx = (25, 0), fill='x')
+        self.buttonframe1.pack(padx=(25, 0), fill='x')
 
     def updateWidget(self):
+        # Permuter les propriétés des boutons (couleur uniquement) entre les deux grilles
         for i in range(5):
             for j in range(10):
-                self.buttonsPlayerTour[i][j].grid_forget()
-                self.buttonsPlayerNonTour[i][j].grid_forget()
+                # Échanger les couleurs entre les deux grilles
+                buttonTour = self.buttonsPlayerTour[i][j]
+                buttonNonTour = self.buttonsPlayerNonTour[i][j]
 
-        # Placer les boutons de buttonsPlayerTour dans buttonframe2 (plateau adverse)
-        for i in range(5):
-            for j in range(10):
-                self.buttonsPlayerNonTour[i][j].grid(row=i, column=j, in_=self.buttonframe2)
-                if self.buttonsPlayerNonTour[i][j].cget("bg") == "red":
-                    self.buttonsPlayerNonTour[i][j].config(bg="red", activebackground="red")
+                # Sauvegarder la couleur actuelle du bouton du plateau adverse
+                tour_bg = buttonTour.cget("bg")
 
-        # Placer les boutons de buttonsPlayerNonTour dans buttonframe1 (votre plateau)
-        for i in range(5):
-            for j in range(10):
-                self.buttonsPlayerTour[i][j].grid(row=i, column=j, in_=self.buttonframe1)
-                if self.buttonsPlayerTour[i][j].cget("bg") == "blue":
-                    self.buttonsPlayerTour[i][j].config(bg="blue", activebackground="blue")
+                # Sauvegarder la couleur actuelle du bouton du joueur
+                nonTour_bg = buttonNonTour.cget("bg")
 
-    def button_change(self, row, col): # Au moment ou une case de plateau est choisie
-            button = self.buttonsPlayerTour[row][col]
-            button.config(bg="blue", activebackground="blue")
-            if self.player == 1:
-                self.player+=1
-            else :
-                self.player-=1
+                # Échanger les propriétés des boutons
+                buttonTour.config(bg=nonTour_bg)
+                buttonNonTour.config(bg=tour_bg)
 
-            self.joueurChanged()
-            self.updateWidget()
+        self.fenetre.update_idletasks()
 
-    def disabledButton(self):
-        for i in self.buttonsPlayerNonTour:
-            for element in i:
-                element["state"] = tk.DISABLED
-        for i in self.buttonsPlayerTour:
-            for element in i:
-                element["state"] = tk.NORMAL
-            self.start = True
+    def button_change(self, row, col):
+        # Lorsque le joueur clique sur un bouton
+        button = self.buttonsPlayerTour[row][col]
+        if self.player == 1:
+            button.config(bg="blue", activebackground="blue")  # Joueur 1
+            self.player = 2  # Changer de joueur
+        else:
+            button.config(bg="red", activebackground="red")  # Joueur 2
+            self.player = 1  # Revenir au joueur 1
 
-    def joueurChanged(self):
-        tmp = self.buttonsPlayerNonTour
-        self.buttonsPlayerNonTour = self.buttonsPlayerTour
-        self.buttonsPlayerTour = tmp
+        # Inverser les données entre les grilles après chaque clic
+        self.updateWidget()
 
     def run(self):
         # Lancer la boucle principale de Tkinter
-        self.disabledButton()
         self.fenetre.mainloop()
 
 
-
+# Démarrer l'application
 app = Board()
 app.run()
